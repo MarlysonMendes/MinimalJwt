648cf5e4fe89ba7a95d6bb97682b271e2e74bdf6
Marlyson Mendes <marlysonmendes@gmail.com>
2022-01-15 14:41:41 -0300

    Conexão com o banco de dados

---
+++ DbAccess/MinimalJwtContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MinimalJwt.Models;
+
+namespace MinimalJwt.DbAccess
+{
+    public class MinimalJwtContext : DbContext
+    {
+
+        public MinimalJwtContext(DbContextOptions options) : base (options)
+        {
+
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+
+    }
+}
